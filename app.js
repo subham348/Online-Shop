a7ded33a120eb8967b229ab6a70e362c7a7ed4f6
subham348 <subhammahato348@gmail.com>
2023-12-03 01:03:39 +0530

    Implemented Templating Engine: ejs

--- app.js
+++ app.js
@@ -2,26 +2,10 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-// const expressHbs = require("express-handlebars");
-
-// Using express-handlebars 7.1.2 instead of taught version 3.0.0
-const { engine } = require("express-handlebars");
 
 const app = express();
 
-// app.engine(
-//   "hbs",
-//   expressHbs({
-//     layoutsDir: "views/layouts/",
-//     defaultLayout: "main-layout",
-//     extname: "hbs",
-//   })
-// );
-
-// main.handlebars file is added in layout folder because of newer
-// version of express-handlebars
-app.engine("hbs", engine());
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -34,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "404" });
 });
 
 app.listen(3000);
