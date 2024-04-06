bc6141568d74007258b9370bb56334c9453bd109
subham348 <subhammahato348@gmail.com>
2024-04-06 15:42:26 +0530

    MongoDB database connection, and create and fetch products

--- util/database.js
+++ util/database.js
@@ -1,9 +1,29 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-// Change password to correct one
-const sequelize = new Sequelize("node-complete", "root", "Your_Password", {
-  dialect: "mysql",
-  host: "localhost",
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://subhammahato348:cK6nnkenqkCueCGR@cluster0.2ow1vby.mongodb.net/"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
